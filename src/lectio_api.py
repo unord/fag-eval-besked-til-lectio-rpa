7d5ceb72fdcd3bce6dbdc6289ac8d3769e903179
Reventlow <gorm@reventlow.com>
2022-12-15 16:21:57 +0100

    initial

--- src/lectio_api.py
+++ src/lectio_api.py
@@ -5,17 +5,22 @@ from decouple import config
 API_ENDPOINT = "https://lectio-fastapi.herokuapp.com/" #link to fastapi
 #API_ENDPOINT = "http://127.0.0.1:8000/" #link to local test fastapi
 
+def send_to_lectio_json_clean_string(this_string: str) -> str:
+    this_string = this_string.replace("\n", "##n")
+    this_string = this_string.replace("æ", "##ae")
+    this_string = this_string.replace("ø", "##oe")
+    this_string = this_string.replace("å", "##aa")
+    this_string = this_string.replace("Æ", "##AE")
+    this_string = this_string.replace("Ø", "##OE")
+    this_string = this_string.replace("Å", "##AA")
+    return this_string
+
 def lectio_send_msg(lectio_school_id: int, lectio_user: str, lectio_password: str, send_to :str, subject: str, msg: str, msg_can_be_replied: bool):
     url = API_ENDPOINT+"message_send/"
     #print(url)
 
-    msg = msg.replace("\n", "##n")
-    msg = msg.replace("æ", "##ae")
-    msg = msg.replace("ø", "##oe")
-    msg = msg.replace("å", "##aa")
-    msg = msg.replace("Æ", "##AE")
-    msg = msg.replace("Ø", "##OE")
-    msg = msg.replace("Å", "##AA")
+    msg = send_to_lectio_json_clean_string(msg)
+    subject = send_to_lectio_json_clean_string(subject)
 
     headers = CaseInsensitiveDict()
     headers["accept"] = "application/json"
