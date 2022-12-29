0897d5119e065bad98322edb515d8f2b973c5d4d
Gorm Reventlow <gorm@reventlow.com>
2022-12-29 08:20:01 +0100

    changed online_eval_api.py

--- src/online_eval_api.py
+++ src/online_eval_api.py
@@ -2,7 +2,7 @@ import requests
 from requests.structures import CaseInsensitiveDict
 
 #API_ENDPOINT = "http://10.127.195.100:8000/" #link to fastapi rpa-03 windows server
-API_ENDPOINT = "http://10.126.225.150:8000/" #link to fastapi rpa-04 linux server
+API_ENDPOINT = "http://10.126.225.150:8081/" #link to fastapi rpa-04 linux server
 
 
 '''
