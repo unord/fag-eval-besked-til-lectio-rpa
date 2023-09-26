374bfb39460c2e378b59c142e6e3a1ae2a89fea5
Gorm Reventlow <gorm@reventlow.com>
2023-09-26 10:18:45 +0200

    update api to support utf-8

--- src/lectio_api.py
+++ src/lectio_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from requests.structures import CaseInsensitiveDict
 from decouple import config
 
@@ -7,37 +8,41 @@ API_ENDPOINT = "https://lectio-fastapi.herokuapp.com/" #link to fastapi
 
 def send_to_lectio_json_clean_string(this_string: str) -> str:
     this_string = this_string.replace("\n", "##n")
-    this_string = this_string.replace("æ", "##ae")
-    this_string = this_string.replace("ø", "##oe")
-    this_string = this_string.replace("å", "##aa")
-    this_string = this_string.replace("Æ", "##AE")
-    this_string = this_string.replace("Ø", "##OE")
-    this_string = this_string.replace("Å", "##AA")
     print(this_string)
     return this_string
 
-def lectio_send_msg(lectio_school_id: int, lectio_user: str, lectio_password: str, send_to :str, subject: str, msg: str, msg_can_be_replied: bool):
-    url = API_ENDPOINT+"message_send/"
-    #print(url)
 
-    send_to = send_to_lectio_json_clean_string(send_to)
-    msg = send_to_lectio_json_clean_string(msg)
-    subject = send_to_lectio_json_clean_string(subject)
+def lectio_send_msg(
+        lectio_school_id: int,
+        lectio_user: str,
+        lectio_password: str,
+        send_to: str,
+        subject: str,
+        msg: str,
+        msg_can_be_replied: bool
+):
+    url = f"{API_ENDPOINT}message_send/"
 
-    headers = CaseInsensitiveDict()
-    headers["accept"] = "application/json"
-    headers["Content-Type"] = "application/json"
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
 
-    payload = '{"lectio_school_id": "' + str(lectio_school_id)
-    payload = payload + '", "lectio_user": "' + lectio_user
-    payload = payload + '", "lectio_password": "' + lectio_password
-    payload = payload + '", "send_to": "' + send_to
-    payload = payload + '", "subject": "' + subject
-    payload = payload + '", "msg": "' + msg
-    payload = payload + '", "msg_can_be_replied": "' + str(msg_can_be_replied) + '"}'
+    payload = json.dumps({
+        "lectio_school_id": lectio_school_id,
+        "lectio_user": lectio_user,
+        "lectio_password": lectio_password,
+        "send_to": send_to,
+        "subject": subject,
+        "msg": msg,
+        "msg_can_be_replied": msg_can_be_replied
+    })
 
-    resp_post = requests.post(url, data=payload, headers=headers)
-    return resp_post
+    resp_post = requests.post(url, json=payload, headers=headers)
+
+    resp_post.encoding = 'utf-8'
+
+    return resp_post.text
 
 def main():
     lectio_school_id = 235
@@ -45,7 +50,7 @@ def main():
     lectio_password = config('LECTIO_RPA_PASSWORD')
     send_to = 'RPA holdet'
     subject = 'test subject'
-    msg = 'test msg'
+    msg = 'test msg  æøå öäü ï'
     msg_can_be_replied = False
     print(lectio_send_msg(lectio_school_id, lectio_user, lectio_password, send_to, subject, msg, msg_can_be_replied).text)
 
